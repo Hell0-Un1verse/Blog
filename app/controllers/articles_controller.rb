cc559877de8bb00b3002509613ede1b1a633b869
Greg Smith <m226090@usna.edu>
2022-01-30 01:28:47 -0500

    Finished RoR

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,32 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all 
   end
-end
+  def show
+    @article = Article.find(params[:id])
+  end
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end 
+  private
+  def article_params
+    params.require(:article).permit(:title, :body, :status)
+  end
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body, :status)
+    end
+
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
+end 
